99c3f5252ed91054f9d2a94d127126bf913d762d
CHAN Lok Tung <ltchanah@connect.ust.hk>
2022-11-16 20:37:46 +0800

    pin change

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -374,7 +374,7 @@ static void MX_TIM3_Init(void)
   htim3.Instance = TIM3;
   htim3.Init.Prescaler = 720-1;
   htim3.Init.CounterMode = TIM_COUNTERMODE_UP;
-  htim3.Init.Period = 200-1;
+  htim3.Init.Period = 1000-1;
   htim3.Init.ClockDivision = TIM_CLOCKDIVISION_DIV1;
   htim3.Init.AutoReloadPreload = TIM_AUTORELOAD_PRELOAD_DISABLE;
   if (HAL_TIM_Base_Init(&htim3) != HAL_OK)
